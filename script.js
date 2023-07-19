3ab3cd8d781121c74efe28de9a09326493cf68d0
Gautham Chalapathy <gautham.c29@gmail.com>
2023-07-19 19:46:13 -0300

    Initial Commit
    Initial commit that contains the complete index.html, complete style.css and partially complete script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+//initial references
+const letterContainer = document.getElementById("letter-container");
+const optionsContainer = document.getElementById("options-container");
+const userInputSection = document.getElementById("user-input-section");
+const newGameContainer = document.getElementById("new-game-container");
+const newGameButton = document.getElementById("new-game-button");
+const canvas = document.getElementById("canvas");
+const resultText = document.getElementById("result-text");
+
+let options = {
+    fruits: ["apple", "blueberry", "mandarin", "pineapple", "pomegranate", "watermelon"],
+    animals: ["hedgehog", "rhinoceros", "squirrel", "panther", "walrus", "zebra"],
+    countries: [
+        "India",
+        "Hungary",
+        "Kyrgystan",
+        "Zimbabwe",
+        "Dominica"
+    ],
+}
+
+//count
+let winCount = 0;
+let count = 0;
+let chosenWord = "";
+
+//display option buttons
+const displayOptions = () => {
+    optionsContainer.innerHTML += `<h3>Please Select an Option</h3>`
+    let buttonCon = document.createElement("div");
+    for (let value in options) {
+        buttonCon.innerHTML += `<button class="options" onclick="generateWord('${value}')">${value}</button>`
+    }
+    optionsContainer.appendChild(buttonCon);
+}
+
+//block all the buttons 
